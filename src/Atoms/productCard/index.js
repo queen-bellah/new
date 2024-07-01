c8d04cae265e12a37bb087f376510cf599d3f966
queen-bellah <umurerwaqueenbella@gmail.com>
2024-07-01 21:33:55 +0300

    added

--- src/Atoms/productCard/index.js
+++ src/Atoms/productCard/index.js
@@ -1,11 +1,11 @@
 const ProductCard = ({image, category,price}) =>{
     return(
-        <>
+        <div className="card">
         <img src={image} alt="profile"/>
         <h2>{category}</h2>
         <h2>{price}</h2>
 
-        </>
+        </div>
     )
 }
 export default ProductCard;
